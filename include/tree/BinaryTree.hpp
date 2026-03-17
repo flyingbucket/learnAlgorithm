5f0caa6e51936255716c482feed1b0e58da6c24d
flyingbucket <2311067@mail.nankai.edu.cn>
2026-03-17 18:43:26 +0800

    feat(tree): add Binary Tree

---
+++ include/tree/BinaryTree.hpp
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_TREE_BINARYTREE_HPP
+#define INCLUDE_TREE_BINARYTREE_HPP
+#include <cstdlib>
+typedef struct {
+  void* arr;
+  size_t elem_size;
+  int capacity;
+  int size;
+} CBTree;
+
+#endif  // !INCLUDE_TREE_BINARYTREE_HPP
