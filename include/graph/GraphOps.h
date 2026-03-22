b9428b0914ab9e237423d2fc61c1ec1b9c30b71f
flyingbucket <2311067@mail.nankai.edu.cn>
2026-03-22 12:59:33 +0800

    feat(graph): add base graph

---
+++ include/graph/GraphOps.h
@@ -0,0 +1,54 @@
+#ifndef INCLUDE_GRAPH_GRAPAOPS_H
+#define INCLUDE_GRAPH_GRAPAOPS_H
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int VertexId;
+
+typedef struct GraphQueryOps {
+  bool (*adjacent)(void* G, VertexId v1, VertexId v2);
+  int (*first_neighbor)(void* G, VertexId v);
+  int (*next_neighbor)(void* G, VertexId v, VertexId w);
+} GraphQueryOps;
+
+typedef struct GraphMutateOps {
+  VertexId (*add_vert)(void* G);
+  bool (*delete_vert)(void* G, VertexId v);
+  bool (*add_edge)(void* G, VertexId v1, VertexId v2);
+  bool (*remove_edge)(void* G, VertexId v1, VertexId v2);
+} GraphMutateOps;
+
+typedef enum {
+  WEIGHT_INT,
+  WEIGHT_DOUBLE,
+  WEIGHT_FLOAT,
+} WeightType;
+
+typedef struct {
+  WeightType type;
+  union {
+    int i;
+    double d;
+    float f;
+  } value;
+} Weight;
+
+typedef struct WeightedGraphOps {
+  Weight (*update_edge_weight)(void* G, VertexId v1, VertexId v2, Weight w);
+  Weight (*get_edge_weight)(void* G, VertexId v1, VertexId v2);
+} WeightedGraphOps;
+
+typedef struct {
+  const GraphQueryOps* qops;
+  const GraphMutateOps* mops;
+  const WeightedGraphOps* wops;
+} BaseGraph;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // !INCLUDE_GRAPH_GRAPAOPS_H
